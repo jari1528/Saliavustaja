4cd76a9b7bec0ff7b5d782396bf7fcae7665e4f5
Irina <irina.keinanen@student.hamk.fi>
2016-01-05 10:15:05 -0800

    otsikkoa muutettu

--- Saliavustaja/Saliavustaja/Saliavustaja.Designer.cs
+++ Saliavustaja/Saliavustaja/Saliavustaja.Designer.cs
@@ -32,11 +32,12 @@
             // 
             // Saliavustaja
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(379, 322);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Saliavustaja";
-            this.Text = "Saliavustaja";
+            this.Text = "Saliavustaja testi";
             this.ResumeLayout(false);
 
         }
