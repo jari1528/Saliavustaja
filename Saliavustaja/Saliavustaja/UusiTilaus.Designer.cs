b77f09fadac8e6c42b7d7ad3ed980cc357be409b
Irina <irina.keinanen@student.hamk.fi>
2016-01-06 06:22:39 -0800

    Loppujen labelien lisäys

--- Saliavustaja/Saliavustaja/UusiTilaus.Designer.cs
+++ Saliavustaja/Saliavustaja/UusiTilaus.Designer.cs
@@ -37,7 +37,12 @@
             this.PoistaAteriaButton = new System.Windows.Forms.Button();
             this.PeruTilausButton = new System.Windows.Forms.Button();
             this.LisaaTilausButton = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
+            this.VerotonHintaLabel = new System.Windows.Forms.Label();
+            this.VeronOsuusLabel = new System.Windows.Forms.Label();
+            this.KokonaishintaLabel = new System.Windows.Forms.Label();
+            this.VerotonSummaLabel = new System.Windows.Forms.Label();
+            this.VeroSummaLabel = new System.Windows.Forms.Label();
+            this.TilausSummaLabel = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -126,21 +131,72 @@
             this.LisaaTilausButton.Text = "Lisää tilaus";
             this.LisaaTilausButton.UseVisualStyleBackColor = true;
             // 
-            // label2
+            // VerotonHintaLabel
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(48, 608);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(46, 17);
-            this.label2.TabIndex = 9;
-            this.label2.Text = "label2";
+            this.VerotonHintaLabel.AutoSize = true;
+            this.VerotonHintaLabel.Location = new System.Drawing.Point(47, 590);
+            this.VerotonHintaLabel.Name = "VerotonHintaLabel";
+            this.VerotonHintaLabel.Size = new System.Drawing.Size(93, 17);
+            this.VerotonHintaLabel.TabIndex = 9;
+            this.VerotonHintaLabel.Text = "Veroton hinta";
+            // 
+            // VeronOsuusLabel
+            // 
+            this.VeronOsuusLabel.AutoSize = true;
+            this.VeronOsuusLabel.Location = new System.Drawing.Point(47, 616);
+            this.VeronOsuusLabel.Name = "VeronOsuusLabel";
+            this.VeronOsuusLabel.Size = new System.Drawing.Size(88, 17);
+            this.VeronOsuusLabel.TabIndex = 10;
+            this.VeronOsuusLabel.Text = "Veron osuus";
+            this.VeronOsuusLabel.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // KokonaishintaLabel
+            // 
+            this.KokonaishintaLabel.AutoSize = true;
+            this.KokonaishintaLabel.Location = new System.Drawing.Point(47, 642);
+            this.KokonaishintaLabel.Name = "KokonaishintaLabel";
+            this.KokonaishintaLabel.Size = new System.Drawing.Size(160, 17);
+            this.KokonaishintaLabel.TabIndex = 11;
+            this.KokonaishintaLabel.Text = "Tilauksen kokonaishinta";
+            // 
+            // VerotonSummaLabel
+            // 
+            this.VerotonSummaLabel.AutoSize = true;
+            this.VerotonSummaLabel.Location = new System.Drawing.Point(442, 590);
+            this.VerotonSummaLabel.Name = "VerotonSummaLabel";
+            this.VerotonSummaLabel.Size = new System.Drawing.Size(44, 17);
+            this.VerotonSummaLabel.TabIndex = 12;
+            this.VerotonSummaLabel.Text = "00,00";
+            // 
+            // VeroSummaLabel
+            // 
+            this.VeroSummaLabel.AutoSize = true;
+            this.VeroSummaLabel.Location = new System.Drawing.Point(442, 616);
+            this.VeroSummaLabel.Name = "VeroSummaLabel";
+            this.VeroSummaLabel.Size = new System.Drawing.Size(44, 17);
+            this.VeroSummaLabel.TabIndex = 13;
+            this.VeroSummaLabel.Text = "00,00";
+            // 
+            // TilausSummaLabel
+            // 
+            this.TilausSummaLabel.AutoSize = true;
+            this.TilausSummaLabel.Location = new System.Drawing.Point(442, 642);
+            this.TilausSummaLabel.Name = "TilausSummaLabel";
+            this.TilausSummaLabel.Size = new System.Drawing.Size(44, 17);
+            this.TilausSummaLabel.TabIndex = 14;
+            this.TilausSummaLabel.Text = "00,00";
             // 
             // UusiTilaus
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1089, 681);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TilausSummaLabel);
+            this.Controls.Add(this.VeroSummaLabel);
+            this.Controls.Add(this.VerotonSummaLabel);
+            this.Controls.Add(this.KokonaishintaLabel);
+            this.Controls.Add(this.VeronOsuusLabel);
+            this.Controls.Add(this.VerotonHintaLabel);
             this.Controls.Add(this.LisaaTilausButton);
             this.Controls.Add(this.PeruTilausButton);
             this.Controls.Add(this.PoistaAteriaButton);
@@ -169,6 +225,11 @@
         private System.Windows.Forms.Button PoistaAteriaButton;
         private System.Windows.Forms.Button PeruTilausButton;
         private System.Windows.Forms.Button LisaaTilausButton;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label VerotonHintaLabel;
+        private System.Windows.Forms.Label VeronOsuusLabel;
+        private System.Windows.Forms.Label KokonaishintaLabel;
+        private System.Windows.Forms.Label VerotonSummaLabel;
+        private System.Windows.Forms.Label VeroSummaLabel;
+        private System.Windows.Forms.Label TilausSummaLabel;
     }
 }
--- Saliavustaja/Saliavustaja/UusiTilaus.cs
+++ Saliavustaja/Saliavustaja/UusiTilaus.cs
@@ -26,5 +26,10 @@ namespace Saliavustaja
         {
 
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
