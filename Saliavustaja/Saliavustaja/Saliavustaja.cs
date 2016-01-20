7bc709c681dbf5d8efdcea28ff42bfaf1cde5a2e
jari1528 <jari1528@student.hamk.fi>
2016-01-21 00:22:24 +0200

    Koodia..

--- Saliavustaja/Saliavustaja/Saliavustaja.cs
+++ Saliavustaja/Saliavustaja/Saliavustaja.cs
@@ -23,11 +23,34 @@ namespace Saliavustaja
         // tilaustietokanta
         List<Tilaus> tilauskanta = new List<Tilaus>();
 
+        bool TilausKesken;
+
         public Saliavustaja()
         {
             InitializeComponent();
         }
 
+        // metodi suoritetaan kun ikkuna avautuu
+        private void Saliavustaja_Load(object sender, EventArgs e)
+        {
+            // ohjelman avauksessa ei ole uutta tilausta auki
+            TilausKesken = false;
+            TarkistaTila();
+
+            // jos tietokantatiedosto löytyy niin yritetään ladata
+            if (File.Exists(tietokanta))
+            {
+                // tietokannan lataus, jos virhe niin lopetetaan ohjelma
+                if ((tilaushallinta.LataaTilauskanta(tilauskanta, tietokanta)) == false)
+                {
+                    MessageBox.Show(Path.GetFullPath(tietokanta) +
+                    "\nLoading database failed, exiting!", "Error");
+                    this.Close();
+                }
+            }           
+        }
+
+        // tarpeettomat --->
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
@@ -36,6 +59,44 @@ namespace Saliavustaja
         private void RuokalistaLaatikko_SelectedIndexChanged(object sender, EventArgs e)
         {
 
+        }
+        // <--- tarpeettomat
+
+        private void TarkistaTila()
+        {
+            if (TilausKesken == false)
+            {
+                LisaaRiviButton.Enabled = false;
+                PoistaRiviButton.Enabled = false;
+                PeruTilausButton.Enabled = false;
+                LisaaTilausButton.Enabled = false;
+            }
+            else if (TilausKesken == true)
+            {
+                LisaaRiviButton.Enabled = true;
+                PoistaRiviButton.Enabled = true;
+                PeruTilausButton.Enabled = true;
+                LisaaTilausButton.Enabled = true;
+            }
+        }
+
+        private void UusiTilausButton_Click(object sender, EventArgs e)
+        {
+            // jos tilaus jo kesken, button ei tee mitään
+            if (TilausKesken == false)
+            {
+                TilausKesken = true;
+                TarkistaTila();
+            }
+            else if (TilausKesken == true)
+            {
+                TilausKesken = false;
+                TarkistaTila();
+            }
+
+            //testing
+            //string[] uusirivi = new string[] { "Erkki esimerkki", "13.45", "2" };
+            //TilausRivit.Rows.Add(uusirivi);
         }
     }
 }
--- Saliavustaja/Saliavustaja/Tilaushallinta.cs
+++ Saliavustaja/Saliavustaja/Tilaushallinta.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+//
+using System.Windows.Forms;
 
 namespace Saliavustaja
 {
@@ -10,5 +12,6 @@ namespace Saliavustaja
     {
         // luokka tilausten hallintaan
         public bool TilausKesken;
+
     }
 }
